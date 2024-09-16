31eac8463b4a8da36627970dd7ac6788cf13ab56
James Stuart Shaw <penguinoneshaw@gmail.com>
2024-09-16 18:25:57 +0100

    Add nix nil filetypes

--- nvim/ftplugin/nix.lua
+++ nvim/ftplugin/nix.lua
@@ -13,6 +13,7 @@ local root_files = {
 vim.lsp.start {
   name = 'nil_ls',
   cmd = { 'nil' },
+  filetype = { 'nix' },
   root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
   capabilities = require('user.lsp').make_client_capabilities(),
 }
